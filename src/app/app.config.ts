1c89905495a45a6b0a203b8dcb8190496c4c1571
Geovanny Aaya Jimenez <geoviolink@gmail.com>
2025-06-02 14:28:12 -0600

    Se descarga angularMaterial y primeNG para aplicar distintas configuraciones de estilo al sitio.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { LearnComponent } from './pages/learn/learn.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { ContactComponent } from './pages/contact/contact.component';
-
+import { providePrimeNG } from 'primeng/config'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeng/themes/aura';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'learn', component: LearnComponent },
@@ -12,5 +14,16 @@ const routes: Routes = [
 ];
 
 export const appConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(), // Agregando animaciones
+    providePrimeNG({ 
+     theme: {
+        preset: Aura,
+       options: {
+            darkModeSelector: false || 'none'
+        }
+      }
+    })
+  ],
 };
